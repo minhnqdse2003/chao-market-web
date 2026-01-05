afa7d5accb51b331db53aa8c7c75170b58e2dbdf
minhnqdse2003 <minhnqdse2003@gmail.com>
2026-01-05 11:28:08 +0700

    chore: update env secret

--- src/lib/sepay-client.ts
+++ src/lib/sepay-client.ts
@@ -2,6 +2,6 @@ import { SePayPgClient } from 'sepay-pg-node';
 
 export const sepayClient = new SePayPgClient({
     env: 'sandbox',
-    merchant_id: process.env.SEPAY_MERCHANT_ID!,
-    secret_key: process.env.SEPAY_SECRET_KEY!,
+    merchant_id: process.env.SEPAY_MERCHANT_ID ?? '',
+    secret_key: process.env.SEPAY_SECRET_KEY! ?? '',
 });
